5d5cf06ddaa5a0907dd0901b680ca88363ad5437
andiichan <biemecs@yahoo.com>
2018-03-24 15:01:49 +0000

    Add extra test for about page

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -11,6 +11,12 @@ describe "Pages Controller" do
   describe "About" do
     it "should be accessible" do
       visit about_path
+      expect(page.status_code).to eq(200)
     end
-  end
+
+    it "has h1 text" do
+      visit about_path
+      expect(page).to have_content("This is the about page")
+    end
+   end
 end
